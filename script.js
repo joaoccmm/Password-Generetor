bad2c5364ad91b2e721c403ac5cbe335d8e115cc
joaoccmm <87348459+joaoccmm@users.noreply.github.com>
2024-03-07 16:45:14 -0300

    Estilo e funcionalidade

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
-let sliderElement = document.querySelector("#slider");
-let buttonElement = document.querySelector("#button");
- 
-let valuElement = document.querySelector("#valor");
-let passwordElement = document.querySelector("#password");
+const pwdInputsChars = document.getElementById('pwdInputsChars').getElementsByTagName('input');
+const quantityContent = document.getElementById('quantityContent');
+const pwdRangeStatus = document.getElementById('pwdRangeStatus');
+const pwdRange = document.getElementById('pwdRange');
 
-let containerElement = document.querySelector("#container-password");
+const password = {
+  value: '',
+  length: 15,
+  characters: {
+      numbers: '0123456789',
+      special: '?/~^{}[]!@#$%&*()_-+=.,:;',
+      lowercase: 'abcdefghijklmnopqrstuvwxyz',
+      uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  },
+  generate(chars) {
+      let pwd = '';
 
-let sets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ1234567890";
-let newPassword = "";
+      this.length = this.length < 4 || this.length > 30 ? 10 : this.length;
 
-valuElement.innerHTML = sliderElement.value;
+      chars = chars || Object.values(this.characters).join();
 
-sliderElement.oninput = function(){
-  valuElement.innerHTML = this.value;
+      for (let i = 0; i < this.length; i++) {
+          pwd += chars[Math.floor(Math.random() * chars.length)];
+      }
+
+      this.value = pwd;
+      return pwd;
+  }
 }
 
-function generatePassword(){
-  alert("Vamos gerar a senha")
+function pwdGenerator() {
+
+  let chars = '';
+
+  for (let i = 0; i < pwdInputsChars.length; i++) {
+      if (pwdInputsChars[i].checked) {
+          chars += password.characters[pwdInputsChars[i].name];
+      }
+  }
+
+  password.length = pwdRange.value;
+  quantityContent.textContent = password.generate(chars);
 }
+
+pwdRange.addEventListener('input', (evt) => {
+  pwdRangeStatus.textContent = evt.currentTarget.value;
+})
+
+pwdRange.addEventListener('change', pwdGenerator);
+for (let i = 0; i < pwdInputsChars.length; i++) {
+  pwdInputsChars[i].addEventListener('change', pwdGenerator);
+}
+
+pwdGenerator();
+
+const btnCopy = document.getElementById('btnCopy');
+btnCopy.addEventListener('click', () => {
+  navigator.clipboard.writeText(password.value)
+  btnCopy.textContent = 'Copiado!';
+  setTimeout(() => {
+      btnCopy.textContent = 'Copiar';
+  }, 2000);
+})
